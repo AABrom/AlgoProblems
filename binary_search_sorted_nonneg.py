ca05c07358191e31009af46f13702d745882ada3
Anastasia Bromberg <137691009+AABrom@users.noreply.github.com>
2024-12-01 17:27:12 +0300

    Update binary_search_sorted_nonneg.py

--- binary_search_sorted_nonneg.py
+++ binary_search_sorted_nonneg.py
@@ -1,7 +1,7 @@
 # if target in sorted non-negative array
 
 def binary_search(input_array, target):
-    left_idx, right_idx = 0, 0
+    left_idx, right_idx = 0, len(input_array)
     while left_idx<right_idx:
         mid_idx = (left_idx+right_idx) // 2
         if input_array[mid_idx] == target:
