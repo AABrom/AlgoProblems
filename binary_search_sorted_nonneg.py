0022c3790349a6552fc09e2d8fcc3d518fd52e5e
Anastasia Bromberg <137691009+AABrom@users.noreply.github.com>
2024-12-01 17:10:07 +0300

    Add files via upload

---
+++ binary_search_sorted_nonneg.py
@@ -0,0 +1,13 @@
+# if target in sorted non-negative array
+
+def binary_search(input_array, target):
+    left_idx, right_idx = 0, 0
+    while left_idx<right_idx:
+        mid_idx = (left_idx+right_idx) // 2
+        if input_array[mid_idx] == target:
+            return True
+        elif input_array[mid_idx] < target:
+            left_idx=mid_idx+1
+        else :
+            right_idx = mid_idx
+    return False
